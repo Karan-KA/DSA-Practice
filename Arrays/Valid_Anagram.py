cf143db6dd9b930d3598227b2807bdcba5e623e4
karan <karan@example.com>
2026-03-21 10:50:32 +0530

    Array (Contains Duplicates)

---
+++ Arrays/Valid_Anagram.py
@@ -0,0 +1,21 @@
+# Problem: Valid Anagram
+# Time complexity: 
+# Space Complexity: 
+# Pattern: Hash Map
+
+class Solution:
+    def isAnagram(self, s, t):
+        map_s = {}
+        map_t = {}
+
+        if len(s) != len(t):
+            return False
+        
+        for char in s:
+            map_s[char] = map_s.get(char, 0) + 1
+            map_t[char] = map_t.get(char, 0) + 1
+
+        if map_s == map_t:
+            return True
+        else:
+            return False
